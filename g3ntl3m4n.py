a399ba3f222c3c39fe642b48cc0413e49aeefb4a
Delwser <87214159+Delwser@users.noreply.github.com>
2024-04-09 19:42:46 -0300

    Update g3ntl3m4n.py

--- g3ntl3m4n.py
+++ g3ntl3m4n.py
@@ -1,35 +1,3 @@
-import os 
-import random
-import shutil
-import string
-
-while True:
-    os.makedirs("DarkParasites" "a" , exist_ok=True)
-    with open(f"DarkParasites/bypass.txt") as arquivo:
-         for _ in range(90000):
-              arquivo.write(random.choice(string.ascii_letters + string.digits))
-              arquivo.close()
-
-    origem = "DarkParisites/bypass.txt"
-    destino = 'Desktop/'
-    shutil.copy(origem, destino)
-
-    os.makedirs("Th3_g3ntl3m4n" , exist_ok=True)
-    with open(f"Th3_g3ntl3m4n/t@nd3r@.txt") as arquivo:
-         for _ in range(90000):
-              arquivo.write(random.choice(string.ascii_letters + string.digits))
-              arquivo.close()
-
-    origem_two = "Th3_g3ntl3m4n/t@nd3r@.txt"
-    shutil.copy(origem_two, destino)
-
-    os.makedirs("J0aninh@s" , exist_ok=True)
-    with open(f"J0aninh@s/fsociety.txt") as arquivo:
-         for _ in range(90000):
-              arquivo.write(random.choice(string.ascii_letters + string.digits))
-              arquivo.close()
-    origem_three = "J0aninh@s/fsociety.txt"
-    shutil.copy(origem_three, destino)
 
 
     
